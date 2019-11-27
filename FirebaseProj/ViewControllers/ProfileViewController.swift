bf8011dcf97d782ec4db4b9e98b68ad514617509
Kyang646 <kimballyang@pursuit>
2019-11-26 20:05:28 -0500

    More UI stuff

--- FirebaseProj/ViewControllers/ProfileViewController.swift
+++ FirebaseProj/ViewControllers/ProfileViewController.swift
@@ -34,9 +34,14 @@ class ProfileViewController: UIViewController {
     
     lazy var dispName: UILabel = {
         let dispLabel = UILabel()
-        dispLabel.layer.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        dispLabel.layer.backgroundColor = .none
         dispLabel.text = "James Hetfield"
-        dispLabel.font = UIFont(name: "Palatino-Bold", size: 25)
+        dispLabel.textColor = .white
+        dispLabel.adjustsFontSizeToFitWidth = true
+        dispLabel.shadowOffset = CGSize(width: 2, height: 2)
+        dispLabel.layer.shadowRadius = 4
+        dispLabel.shadowColor = .black
+        dispLabel.font = UIFont(name: "Palatino-Bold", size: 40)
         dispLabel.translatesAutoresizingMaskIntoConstraints = false
         dispLabel.textAlignment = .center
         view.addSubview(dispLabel)
@@ -45,19 +50,49 @@ class ProfileViewController: UIViewController {
     
     lazy var editButton: UIButton = {
         let eButton = UIButton()
-        eButton.layer.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        eButton.layer.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        eButton.setTitle("Edit", for: .normal)
+        eButton.setTitleColor(.white, for: .normal)
+        eButton.layer.cornerRadius = 10
+        eButton.layer.shadowOffset = CGSize(width: 5, height: 5)
+        eButton.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        eButton.layer.shadowRadius = 4
+        eButton.layer.shadowOpacity = 0.6
         eButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(eButton)
        return eButton
     }()
     
+    lazy var eLabel: UILabel = {
+        let email = UILabel()
+        email.layer.backgroundColor = .none
+        email.text = "jameshetfield@yosemite.com"
+        email.textColor = .white
+        email.adjustsFontSizeToFitWidth = true
+        email.shadowOffset = CGSize(width: 2, height: 2)
+        email.layer.shadowRadius = 4
+        email.shadowColor = .black
+        email.font = UIFont(name: "Palatino-Bold", size: 22)
+        email.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(email)
+        return email
+    }()
+    
     lazy var numOfImages: UILabel = {
         let numLabel = UILabel()
-        numLabel.layer.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        numLabel.layer.backgroundColor = .none
+        numLabel.text = "You have submitted 18 images."
+        numLabel.textColor = .white
+        numLabel.adjustsFontSizeToFitWidth = true
+        numLabel.shadowOffset = CGSize(width: 2, height: 2)
+        numLabel.layer.shadowRadius = 4
+        numLabel.shadowColor = .black
+        numLabel.font = UIFont(name: "Palatino-Bold", size: 22)
         numLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(numLabel)
         return numLabel
     }()
+    
     
     /*
      Profile label
@@ -76,7 +111,7 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         setDaProfileCons()
         
     }
@@ -101,7 +136,22 @@ class ProfileViewController: UIViewController {
             dispName.topAnchor.constraint(equalTo: profImage.bottomAnchor, constant: 20),
             dispName.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             dispName.heightAnchor.constraint(equalToConstant: 60),
-            dispName.widthAnchor.constraint(equalToConstant: 330)
+            dispName.widthAnchor.constraint(equalTo: view.widthAnchor),
+            
+            editButton.topAnchor.constraint(equalTo: dispName.bottomAnchor, constant: 20),
+            editButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            editButton.heightAnchor.constraint(equalToConstant: 30),
+            editButton.widthAnchor.constraint(equalToConstant: 100),
+            
+            eLabel.topAnchor.constraint(equalTo: editButton.bottomAnchor, constant: 30),
+            eLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            eLabel.heightAnchor.constraint(equalToConstant: 30),
+            eLabel.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30),
+            
+            numOfImages.topAnchor.constraint(equalTo: eLabel.bottomAnchor, constant:  5),
+            numOfImages.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            numOfImages.heightAnchor.constraint(equalToConstant: 30),
+            numOfImages.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30)
         
         
         
